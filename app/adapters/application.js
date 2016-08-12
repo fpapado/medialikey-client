88a897aedda5a9103003e8d34b55792285f349b5
fpapado <fgpapado@gmail.com>
2016-08-12 22:17:02 +0300

    Back to old adapter

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,7 +2,4 @@ import DS from 'ember-data';
 import Ember from 'ember';
 
 export default DS.JSONAPIAdapter.extend({
-  pathForType: function(type) {
-    return Ember.String.pluralize(Ember.String.underscore(type));
-  }
 });
