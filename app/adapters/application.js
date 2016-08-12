9b065eba099fb147331900ad0f026145b827d4db
fpapado <fgpapado@gmail.com>
2016-08-12 20:04:43 +0300

    Added adapter for rails backend

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,8 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 export default DS.JSONAPIAdapter.extend({
-  namespace: 'api'
+  pathForType: function(type) {
+    return Ember.String.pluralize(Ember.String.underscore(type));
+  }
 });
