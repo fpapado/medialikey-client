358f49f02dd1dc5625d06128f202710cb9e70773
fpapado <fgpapado@gmail.com>
2016-08-12 20:16:12 +0300

    Minor fix

--- app/controllers/posts/show.js
+++ app/controllers/posts/show.js
@@ -8,12 +8,10 @@ export default Ember.Controller.extend({
       let vote = this.store.createRecord('vote', {
         post: model,
         accepted: true
-      })
+      });
 
       vote.save().then(() => {
-        this.store.peekRecord('post', model.id).get('votes').then((votes) => {
-          console.log('got it');
-        });
+        console.log("Got it!");
       });
     },
     votedown(model) {
@@ -22,7 +20,7 @@ export default Ember.Controller.extend({
       let vote = this.store.createRecord('vote', {
         post: model,
         accepted: false
-      })
+      });
 
       vote.save();
     }
