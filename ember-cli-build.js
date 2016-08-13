08301d30cf4d10a0465f480ead8b1a3ba3f6c1f2
fpapado <fgpapado@gmail.com>
2016-08-13 18:22:52 +0300

    Added fontello and demo

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -7,6 +7,26 @@ module.exports = function(defaults) {
     // Add options here
   });
 
+  app.import('vendor/fontello/css/minder.css');
+  app.import('vendor/fontello/css/minder-codes.css');
+  app.import('vendor/fontello/css/minder-embedded.css');
+
+  app.import('vendor/fontello/font/minder.ttf', {
+    destDir: 'assets/font'
+  });
+  app.import('vendor/fontello/font/minder.eot', {
+    destDir: 'assets/font'
+  });
+  app.import('vendor/fontello/font/minder.svg', {
+    destDir: 'assets/font'
+  });
+  app.import('vendor/fontello/font/minder.woff', {
+    destDir: 'assets/font'
+  });
+  app.import('vendor/fontello/font/minder.woff2', {
+    destDir: 'assets/font'
+  });
+
   app.import('vendor/tachyons.min.css');
 
   return app.toTree();
